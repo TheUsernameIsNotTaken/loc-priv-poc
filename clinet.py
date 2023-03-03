e70af430d0a24fc0d3910719a346ca7a71e23e4e
TheUsernameIsNotTaken <vagnermate@gmail.com>
2023-03-03 14:23:05 +0100

    HAMHAM-2 Added address check

--- clinet.py
+++ clinet.py
@@ -4,7 +4,10 @@
 import socket
 
 client = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-client.connect(("::1", 9999))
+client.connect(("localhost", 9999))
+
+# Return Address
+print(client.getsockname())
 
 client.send("Hello from spaceship!".encode())
 print(client.recv(1024).decode())
